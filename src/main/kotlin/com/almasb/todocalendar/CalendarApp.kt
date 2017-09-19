e8d3a0e5ba82cf40f9080a15b112bfe36e36be05
AlmasB <almaslvl@gmail.com>
2017-09-19 20:22:52 +0100

    added list view and calendar view

--- src/main/kotlin/com/almasb/todocalendar/CalendarApp.kt
+++ src/main/kotlin/com/almasb/todocalendar/CalendarApp.kt
@@ -1,21 +1,39 @@
 package com.almasb.todocalendar
 
 import javafx.application.Application
+import javafx.collections.FXCollections
 import javafx.scene.Parent
-import tornadofx.App
-import tornadofx.View
-import tornadofx.borderpane
+import tornadofx.*
+import java.time.LocalDate
+import javafx.scene.control.DatePicker
+import com.sun.javafx.scene.control.skin.DatePickerSkin
+import javafx.geometry.Pos
+import javafx.scene.Scene
+import javafx.scene.layout.BorderPane
+
+
 
 /**
  *
  *
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
-class CalendarApp : App(CalendarView::class) {
-}
+class CalendarApp : App(CalendarView::class)
 
 class CalendarView : View() {
-    override val root: Parent = borderpane()
+    override val root: Parent = borderpane {
+
+        center {
+            hbox {
+                paddingAll = 15
+
+                listview(FXCollections.observableArrayList<String>())
+
+                val datePickerSkin = DatePickerSkin(DatePicker(LocalDate.now()))
+                add(datePickerSkin.popupContent)
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
